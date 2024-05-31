8ed3fd2da4c76ed92ccd33d77b47216bad9bf2ef
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-05-31 19:48:35 -0400

    feat: implement DTOs for data store and mapping

---
+++ soft-dev-v-api/api/dtos/CareerDTO.cs
@@ -0,0 +1,6 @@
+public class CareerDTO
+{
+  public Guid Id { get; set; }
+  public string Name { get; set; } = "";
+  public int Code { get; set; }
+}
---
+++ soft-dev-v-api/api/dtos/StudentDTO.cs
@@ -0,0 +1,7 @@
+public class StudentDTO
+{
+  public Guid Id { get; set; }
+  public string Name { get; set; } = "";
+  public string Lastname { get; set; } = "";
+  public DateTime Birthdate { get; set; }
+}
