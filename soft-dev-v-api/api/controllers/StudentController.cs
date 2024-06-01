f0fa4ad8d10ef6c5cc28605b817f441edb89abac
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-05-31 20:15:00 -0400

    feat: implement mapping functionality using AutoMapper
    This will help us to transform data between domain models and DTOs in the controller's endpoints which will improve the capability to transform data for responses.

--- soft-dev-v-api/api/controllers/StudentController.cs
+++ soft-dev-v-api/api/controllers/StudentController.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,19 +8,39 @@ using Microsoft.AspNetCore.Mvc;
 public class StudentController : ControllerBase
 {
   private readonly IMediator _mediator;
+  private readonly IMapper _mapper;
 
-  public StudentController(IMediator mediator)
+  public StudentController(IMediator mediator, IMapper mapper)
   {
     _mediator = mediator;
+    _mapper = mapper;
   }
 
-  [HttpGet("IdStudent")]
+  [HttpGet("idStudent")]
   public async Task<IActionResult> Get([Required] Guid idStudent)
   {
-    var middle = new StudentMiddleData(idStudent);
-    var student = await _mediator.Send(middle);
+    var contract = new GetStudentMiddleData(idStudent);
+    var middleResponse = await _mediator.Send(contract);
+    var student = _mapper.Map<StudentDTO>(middleResponse);
 
     return Ok(student);
+  }
+
+  [HttpGet, Route("careers/idStudent")]
+  public async Task<IActionResult> GetCareersPerStudent([Required] Guid idStudent)
+  {
+    var contract = new GetCareersPerStudentMiddleData(idStudent);
+    var middleResponse = await _mediator.Send(contract);
+    var careers = _mapper.Map<IList<CareerDTO>>(middleResponse);
+
+    return Ok(careers);
+  }
+
+  [HttpPost, Route("subscribe")]
+  public async Task<IActionResult> Post([Required] Guid idCareer, Student student)
+  {
+
+    return Ok();
   }
 
   [HttpPost]
